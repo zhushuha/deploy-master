673623a0a2cdc8c632d2f106119d7eea409cbed9
zhushuha <11276740@qq.com>
2017-09-17 01:18:32 +0800

    init repo

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿var http = require("http");
+//var path = require("path");
+//var querystring = require('querystring');
+//var process = require('child_process');
+//var moment = require("moment");
+var express = require("express");
+var app = express();
+
+
+app.get("/", (request, response) => {
+	//var time = moment().format("YYYY-MM-DD HH:mm:ss");
+	//if(request.params.cmd == 'deploy-app') {
+		//process.exec('ls -l',
+		//  function (error, stdout, stderr) {
+		//	if (error !== null) {
+			  //console.log('exec error: ' + error);
+			//}
+			//response.send(JSON.stringify({code : 200, time : time, output : stdout}));
+		//});
+	//}
+});
+
+// init server
+http.createServer(app).listen(10081, () => {
+	console.log("Server started on port", 10081);
+});
