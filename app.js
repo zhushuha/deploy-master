c4966caaaa316829e62291fbd6b6e7ca62494ba9
zhushuha <11276740@qq.com>
2017-09-17 01:27:24 +0800

    depens

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 ﻿var http = require("http");
-//var path = require("path");
-//var querystring = require('querystring');
-//var process = require('child_process');
-//var moment = require("moment");
+var path = require("path");
+var querystring = require('querystring');
+var process = require('child_process');
+var moment = require("moment");
 var express = require("express");
 var app = express();
 
 
 app.get("/", (request, response) => {
-	//var time = moment().format("YYYY-MM-DD HH:mm:ss");
-	//if(request.params.cmd == 'deploy-app') {
-		//process.exec('ls -l',
-		//  function (error, stdout, stderr) {
-		//	if (error !== null) {
-			  //console.log('exec error: ' + error);
-			//}
-			//response.send(JSON.stringify({code : 200, time : time, output : stdout}));
-		//});
-	//}
+	var time = moment().format("YYYY-MM-DD HH:mm:ss");
+	if(request.params.cmd == 'deploy-app') {
+		process.exec('ls -l',
+		 function (error, stdout, stderr) {
+			if (error !== null) {
+			  console.log('exec error: ' + error);
+			}
+			response.send(JSON.stringify({code : 200, time : time, output : stdout}));
+		});
+	}
 });
 
 // init server
